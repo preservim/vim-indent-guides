preservim/vim-indent-guides

commit 3a4f7a6b9967e92cbf4ad2c048751a95d26e17dc

feat: Enable adaption of colors from termguicolors (#143)

autoload/indent_guides.vim
@@ -100,7 +100,7 @@ endfunction
 "
 function! indent_guides#highlight_colors() abort
   if s:auto_colors
-    if has('gui_running') || has('nvim')
+    if has('gui_running') || has('nvim') || has('termguicolors')
       call indent_guides#gui_highlight_colors()
     else
       call indent_guides#basic_highlight_colors()

commit 5a659e83bd10cf2b095ffb4e94edbb1d6fe8f1c6

fix: Use basic colors when termguicolors is off
indent_guides#gui_highlight_colors only sets guifg/guibg and we don't
want that when termguicolors is off.

Fixes: 3a4f7a6b9967 ("feat: Enable adaption of colors from termguicolors (#143)")

autoload/indent_guides.vim
@@ -100,7 +100,7 @@ endfunction
 "
 function! indent_guides#highlight_colors() abort
   if s:auto_colors
-    if has('gui_running') || has('nvim') || has('termguicolors')
+    if has('gui_running') || has('nvim') || (has('termguicolors') && &termguicolors)
       call indent_guides#gui_highlight_colors()
     else
       call indent_guides#basic_highlight_colors()